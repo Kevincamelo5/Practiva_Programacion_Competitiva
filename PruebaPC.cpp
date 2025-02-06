f2573511321c96b4dd27f66e51534dcdb4d71ba6
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2025-02-06 10:14:42 -0600

    Prueba de función de Clion
    Aqui se realiza la prueba de que Clion funciona para correr ejercicios dentro de C++ para programación competitiva

---
+++ PruebaPC.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Kevin L. Camelo S. on 06/02/2025.
+//
+
+#include "PruebaPC.h"
+
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a=5,b=6;
+
+  cout<<a + b<<endl;
+
+  return 0;
+}
---
+++ PruebaPC.h
@@ -0,0 +1,8 @@
+//
+// Created by HP on 06/02/2025.
+//
+
+#ifndef PRUEBAPC_H
+#define PRUEBAPC_H
+
+#endif //PRUEBAPC_H
