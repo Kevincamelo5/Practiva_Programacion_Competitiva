31446576c15182c219a3f97ac77e43a24259bf27
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2025-02-22 21:53:37 -0600

    se aplicaron ejericiocios de colas safari maneja colas dobles y encuesta usa un metodo complejo que no es colas

--- ColaTamalera.cpp
+++ ColaTamalera.cpp
@@ -18,20 +18,25 @@ bool revAtenditdos (int q, queue<int> s) {
 }
 
 int main() {
-  queue<int> q,aux,atendido;
+  queue<int> q,atendido;
+  string orden;
 
   cin >> inv;
 
   while(inv--) {
-    string orden; cin >> orden;
+    cin >> orden;
     if(orden == "FORMA"){
       cin >> tamal;
       q.push(tamal);
     }else if(orden == "ATIENDE"){
-      if(!q.empty() && !atendido.empty()){
+      if(!q.empty()){
         if(revAtenditdos(q.front(), atendido)){
           cout << "NO " << tm << " " << q.front() << endl;
-        }else cout << "SI " << tm << " " << q.front() << endl;
+        }else {
+          cout << "SI " << tm << " " << q.front() << endl;
+          atendido.push(q.front());
+        }
+        q.pop();
       }
     }
   }
